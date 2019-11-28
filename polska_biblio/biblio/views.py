11711846f29e44a553f202e9b1844254fd720855
imoren2x <imoren2x@users.sourceforge.net>
2019-11-28 00:40:25 +0000

    Addition of the change number view and more corrections.
    Signed-off-by: imoren2x <imoren2x@users.sourceforge.net>

--- polska_biblio/biblio/views.py
+++ polska_biblio/biblio/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import get_object_or_404, render, redirect
 from django.conf import settings
 # from django.core.urlresolvers import reverse
 from django.urls import reverse
+from django.urls import resolve
 from . import models, forms
 from django.views.generic import View, ListView
 
@@ -68,7 +69,7 @@ class BookView(View):
 
 class BookEditView(GetReturnURLMixin, View):
     """
-    Create or edit a single TradeModel.
+    Create or edit a single book.
     """
     model = models.Book
     form_class = forms.BookForm
@@ -87,18 +88,25 @@ class BookEditView(GetReturnURLMixin, View):
 
     def get(self, request, *args, **kwargs):
 
+        number_form = None
         book = self.get_object(kwargs)
+        if not book.id:
+            number_form = forms.ChangePKBookForm()
+            number_form.fields['number'].label = "Number"
+
         # Parse initial data manually to avoid setting field values as lists
         initial_data = {k: request.GET[k] for k in request.GET}
 
         form = self.form_class(instance=book, initial=initial_data)
 
+        return_url = reverse('book', args=[book.pk]) if book.pk else reverse('home')
+
         return render(request, self.template_name, {
             'book': book,
             'obj_type': self.model._meta.verbose_name,
             'form': form,
-            # 'return_url': redirect(self.default_return_url, book.pk),
-            'return_url': reverse('home'),
+            'number_form': number_form,
+            'return_url': return_url,
         })
 
     def post(self, request, *args, **kwargs):
@@ -106,11 +114,17 @@ class BookEditView(GetReturnURLMixin, View):
         obj = self.get_object(kwargs)
         print("request.POST", request.POST, "")
         form = self.form_class(request.POST, request.FILES, instance=obj)
+        number_form = forms.ChangePKBookForm(request.POST, request.FILES)
+        valid_form = number_form.is_valid()
+        number = number_form.cleaned_data['number']
+        number_isavailable = models.Book.objects.filter(pk=number).count() == 0
 
-        if form.is_valid():
+        if form.is_valid() and number_isavailable:
             print("VALIDO")
             obj = form.save(commit=False)
             obj_created = not obj.pk
+            if obj_created:
+                obj.pk = number
             obj.save()
             return redirect(self.default_return_url, obj.pk)
         else:
@@ -120,6 +134,68 @@ class BookEditView(GetReturnURLMixin, View):
 
         return render(request, self.template_name, {
             'form': form,
+            'number_form': number_form,
+            'number_invalid': not number_isavailable,
+            'number': number,
+            'return_url': reverse('home'),
+        })
+
+
+class ChangePKBookView(BookEditView):
+    """
+    """
+    model = models.Book
+    form_class = forms.ChangePKBookForm
+    template_name = 'books/book_changepk.html'
+    default_return_url = 'book'
+
+    def get(self, request, *args, **kwargs):
+
+        book = self.get_object(kwargs)
+        # Parse initial data manually to avoid setting field values as lists
+        initial_data = {k: request.GET[k] for k in request.GET}
+
+        if book.id:
+            initial_data.update({'number': book.pk})
+        form = self.form_class(initial=initial_data)
+
+        return_url = reverse('book', args=[book.pk]) if book.pk else reverse('home')
+
+        return render(request, self.template_name, {
+            'book': book,
+            'obj_type': self.model._meta.verbose_name,
+            'form': form,
+            'return_url': return_url,
+        })
+
+    def post(self, request, *args, **kwargs):
+
+        book = self.get_object(kwargs)
+        # print("request.POST", request.POST, "")
+        form = self.form_class(request.POST, request.FILES)
+        valid_form = form.is_valid()
+        new_pk = form.cleaned_data['number']
+        new_pk_isavailable = models.Book.objects.filter(pk=new_pk).count() == 0
+        if valid_form and new_pk_isavailable:
+            # print("VALIDO")
+            # obj = form.save(commit=False)
+            new_book = book
+            book.delete()
+            new_book.pk = new_pk
+            # obj_created = not obj.pk
+            new_book.save()
+            book = new_book
+            return redirect(self.default_return_url, new_book.pk)
+        else:
+            print("NO VALIDO")
+            print("form.errors", form.errors, "")
+            print("form.non_field_errors", form.non_field_errors(), "")
+
+        return render(request, self.template_name, {
+            'form': form,
+            'new_pk': new_pk,
+            'new_pk_invalid': not new_pk_isavailable,
+            'book': book,
             'return_url': reverse('home'),
         })
 
